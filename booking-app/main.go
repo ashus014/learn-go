0a3933182d5c3de2190417b34b78d2d6af252633
Ashu Singh <ashu.ask.singh@gmail.com>
2023-10-01 20:31:02 +0530

    working with varibales

--- booking-app/main.go
+++ booking-app/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Welcome to our conference booking application")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
 	fmt.Println("Get your tickets form here to attend")
+
+	
+	fmt.Println(conferenceName)
 }
