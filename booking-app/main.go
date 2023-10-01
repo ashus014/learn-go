0461cd6a73bcf2697935cb3db55f3e9a34e1a814
Ashu Singh <ashu.ask.singh@gmail.com>
2023-10-01 22:33:06 +0530

    taking user inputs

--- booking-app/main.go
+++ booking-app/main.go
@@ -4,8 +4,10 @@ import "fmt"
 
 func main() {
 	var conferenceName string = "Go conference"
-	const conferenceTickets int = 50
-	var remainingTickets int = 50
+	// syntatial sugar
+	// const conferenceTickets int = 50
+	conferenceTickets := 50
+	var remainingTickets uint = 50
 
 	fmt.Printf("conferenceName is %T type\n conferenceTickets is %T type\n remainingTickets is %T type\n", conferenceName, conferenceTickets, remainingTickets)
 
@@ -15,10 +17,30 @@ func main() {
 
 	fmt.Println(conferenceName)
 
-	var userName string
-	var userTicket int
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket uint
 
-	userName = "ashu"
-	userTicket = 2
-	fmt.Printf("User %v booked %v tickets\n", userName, userTicket)
+	// asking for user input
+	fmt.Print("Enter your first name :: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter your last name :: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter your email name :: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter no. of tickets :: ")
+	fmt.Scan(&userTicket)
+
+	remainingTickets = remainingTickets - userTicket
+
+	fmt.Printf("Thankyou %v %v for booking %v tickets. You'll receive a confirmation email address at %v\n", firstName, lastName, userTicket, email)
+
+	// fmt.Printf("User %v booked %v tickets\n", firstName, userTicket)
+	// fmt.Println("Memory location of userName Variable", &firstName)
+
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
