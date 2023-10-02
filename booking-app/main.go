fc9b9fd1c1ae544c17b2614db74f1e5cc7a54f72
Ashu Singh <ashu.ask.singh@gmail.com>
2023-10-02 12:52:35 +0530

    working with loops

--- booking-app/main.go
+++ booking-app/main.go
@@ -18,39 +18,47 @@ func main() {
 	// fmt.Println(conferenceName)
 
 	// var bookings = [50] string {"Ashu", "Akash", "Anu"}
-	var bookings [50] string
+	// var bookings [50] string
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket uint
+	// using slice, a dynamic list in Go
+	var bookings [] string
 
-	// asking for user input
-	fmt.Print("Enter your first name :: ")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
+	
+		// asking for user input
+		fmt.Print("Enter your first name :: ")
+		fmt.Scan(&firstName)
+	
+		fmt.Print("Enter your last name :: ")
+		fmt.Scan(&lastName)
+	
+		fmt.Print("Enter your email name :: ")
+		fmt.Scan(&email)
+	
+		fmt.Print("Enter no. of tickets :: ")
+		fmt.Scan(&userTicket)
+	
+		remainingTickets = remainingTickets - userTicket
+		// bookings[0] = firstName + " " + lastName
+		bookings = append(bookings, firstName + " " + lastName)
+	
+		fmt.Printf("The whole slice : %v\n", bookings)
+		fmt.Printf("The first value : %v\n", bookings[0])
+		fmt.Printf("Slice type : %T\n", bookings)
+		fmt.Printf("Slice length: %v\n", len(bookings))
+	
+	
+		fmt.Printf("Thankyou %v %v for booking %v tickets. You'll receive a confirmation email address at %v\n", firstName, lastName, userTicket, email)
+	
+		// fmt.Printf("User %v booked %v tickets\n", firstName, userTicket)
+		// fmt.Println("Memory location of userName Variable", &firstName)
+	
+		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	}
 
-	fmt.Print("Enter your last name :: ")
-	fmt.Scan(&lastName)
-
-	fmt.Print("Enter your email name :: ")
-	fmt.Scan(&email)
-
-	fmt.Print("Enter no. of tickets :: ")
-	fmt.Scan(&userTicket)
-
-	remainingTickets = remainingTickets - userTicket
-	bookings[0] = firstName + " " + lastName
-
-	fmt.Printf("The whole array : %v\n", bookings)
-	fmt.Printf("The first value : %v\n", bookings[0])
-	fmt.Printf("Array type : %T\n", bookings)
-	fmt.Printf("Array length: %v\n", len(bookings))
-
-
-	fmt.Printf("Thankyou %v %v for booking %v tickets. You'll receive a confirmation email address at %v\n", firstName, lastName, userTicket, email)
-
-	// fmt.Printf("User %v booked %v tickets\n", firstName, userTicket)
-	// fmt.Println("Memory location of userName Variable", &firstName)
-
-	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+	
 }
